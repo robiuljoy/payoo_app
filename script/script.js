a04fda79ba783494fb9300c20ac9f79de6be9c58
robiuljoy <robiuljoy91@gmail.com>
2025-08-23 16:55:10 +0600

    completed the login page using html,tailwind,js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,22 @@
 // Login button
 
-document.getElementById("login-btn").addEventListener("click", function () {
-  console.log("worked");
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberConverted = parseInt(pinNumberValue);
+
+  if (
+    mobileNumberConverted === mobileNumber &&
+    pinNumberConverted === pinNumber
+  ) {
+    window.location.href = "../home.html";
+  } else {
+    alert("Invalid Password");
+  }
 });
